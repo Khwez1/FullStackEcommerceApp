66772a6de92a93e3cedd9029d0f4b08ff5e40b89
Khwez1 <khwezimbobo@gmail.com>
2025-05-08 01:12:14 +0200

    Validation with zod + drizzle schema

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
 import { createProduct, getProduct, listProducts, updateProduct, deleteProduct } from "./productsController";
+import { validateData } from "../../middleware/validation";
+
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { productsTable } from "../../db/productsSchema";
+
+const createProductSchema = createInsertSchema(productsTable);
+const updateProductSchema = createInsertSchema(productsTable).partial();
 
 const router = Router();
 
 router
     .route('/')
         .get(listProducts)
-        .post(createProduct)
+        .post(validateData(createProductSchema), createProduct)
 router
     .route('/:id')
         .get(getProduct)
-        .patch(updateProduct)
+        .patch(validateData(updateProductSchema), updateProduct)
         .delete(deleteProduct)
 
 export default router
