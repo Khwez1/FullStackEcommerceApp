009172f046f959249f1ccfcb2025024c753c533b
Khwez1 <khwezimbobo@gmail.com>
2025-05-16 11:28:29 +0200

    installed gluestack

--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -1,13 +1,17 @@
+import "@/global.css";
 // app/_layout.tsx
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
   return (
     <View style={{ flex: 1 }}>
       <StatusBar style="auto" />
-      <Stack />
+      <GluestackUIProvider>
+        <Stack screenOptions={{ headerTitleAlign: 'center' }} />
+      </GluestackUIProvider>
     </View>
   );
 }
