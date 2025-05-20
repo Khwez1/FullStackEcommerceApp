03f4d70607f792fc427b8c8ce1382181f729cd54
Khwez1 <khwezimbobo@gmail.com>
2025-05-20 20:54:23 +0200

    Authentication

---
+++ ecommerce-mobile/app/(auth)/login.tsx
@@ -0,0 +1,102 @@
+import { Button, ButtonText } from "@/components/ui/button";
+import { FormControl } from "@/components/ui/form-control";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField, InputIcon, InputSlot } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { EyeIcon, EyeOffIcon } from "@/components/ui/icon";
+import { useState } from "react";
+import { HStack } from "@/components/ui/hstack";
+import { useMutation } from "@tanstack/react-query";
+import { login, signup } from "@/api/auth";
+import { useAuth } from "@/store/authStore";
+import { Redirect } from "expo-router";
+ 
+export default function LoginScreen() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const setUser = useAuth((s) => s.setUser);
+  const setToken = useAuth((s) => s.setToken);
+  const isLoggedIn = useAuth((s) => !!s.token)
+
+  const loginMutation = useMutation({
+    mutationFn: () => login(email, password), 
+    onSuccess: (data) => {
+      console.log('Success, Signed in:', data);
+      if(data.user && data.token){
+        setUser(data.user);
+        setToken(data.token);
+      }
+    },
+    onError: (error) => {
+      console.log('Error:', error);
+    }
+  });
+
+  const signupMutation = useMutation({
+    mutationFn: () => signup(email, password), 
+    onSuccess: (data) => {
+      console.log('Success, Signed up:', data);
+    },
+    onError: (error) => {
+      console.log('Error:', error);
+    }
+  });
+
+  const handleState = () => {
+    setShowPassword((showState) => {
+      return !showState;
+    });
+  };
+
+  if (isLoggedIn) {
+    return <Redirect href={'/'} />
+  }
+
+  console.log(password);
+  console.log(email);
+  
+  return (
+    <FormControl isInvalid={loginMutation.error || signupMutation.error} className="p-4 border rounded-lg max-w-[500px] border-outline-300 m-2 bg-white">
+      <VStack space="xl">
+        <Heading className="text-typography-900">Login</Heading>
+        <VStack space="xs">
+          <Text className="text-typography-500">Email</Text>
+          <Input>
+            <InputField value={email} onChangeText={setEmail} type="text" />
+          </Input>
+        </VStack>
+        <VStack space="xs">
+          <Text className="text-typography-500">Password</Text>
+          <Input className="text-center">
+            <InputField value={password} onChangeText={setPassword} type={showPassword ? "text" : "password"} />
+            <InputSlot className="pr-3" onPress={handleState}>
+              <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+            </InputSlot>
+          </Input>
+        </VStack>
+        <HStack space='sm'>
+          <Button
+            className="flex-1"
+            variant="outline"
+            onPress={() => loginMutation.mutate()}
+          >
+            <ButtonText className="">
+              Sign In
+            </ButtonText>
+          </Button>
+          <Button
+            className="flex-1"
+            onPress={() => signupMutation.mutate()}
+          >
+            <ButtonText className="text-typography-0">
+              Sign Up
+            </ButtonText>
+          </Button>
+        </HStack>
+      </VStack>
+    </FormControl>
+  );
+}
--- ecommerce-mobile/app/_layout.tsx
+++ ecommerce-mobile/app/_layout.tsx
@@ -5,13 +5,17 @@ import { View, Text, Pressable } from "react-native";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Icon } from "@/components/ui/icon";
-import { ShoppingCart } from "lucide-react-native";
+import { ShoppingCart, User } from "lucide-react-native";
 import { useCart } from "@/store/cartStore";
+import { useAuth } from "@/store/authStore";
 
 const queryClinet = new QueryClient()
  
 export default function RootLayout() {
   const cartItemsNum = useCart((state) => state.items.length);
+
+    const isLoggedIn = useAuth((s) => !!s.token)
+  
   return (
     <View style={{ flex: 1, marginTop: 8 }}>
       <StatusBar style="auto" />
@@ -30,6 +34,15 @@ export default function RootLayout() {
               )
             }}
           >
+            <Stack.Screen name="(auth)" options={{ title: 'Shop',
+              headerLeft: () => !isLoggedIn && (
+                <Link href={'/login'} asChild>
+                  <Pressable className="flex-row gap-2">
+                    <Icon as={User} />
+                  </Pressable>
+                </Link>
+              )
+             }} />
             <Stack.Screen name="index" options={{ title: 'Shop' }} />
             <Stack.Screen name="product/[id]" options={{ title: 'Product' }} />
           </Stack>
