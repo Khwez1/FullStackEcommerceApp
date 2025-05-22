9a511e0c0bdcd667afdda44e678c8837807a81ae
Khwez1 <khwezimbobo@gmail.com>
2025-05-22 15:26:07 +0200

    to complete order management page

---
+++ ecommerce-mobile/app/orderManagement.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function orderManagement() {
+  return (
+    <View>
+      <Text>orderManagement</Text>
+    </View>
+  )
+}
